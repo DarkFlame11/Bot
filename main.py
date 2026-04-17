0d36198e14923fc5c63279c34d41180d8cdb39e9
DarkFlame11 <96309406+DarkFlame11@users.noreply.github.com>
2026-04-17 20:17:21 +0400

    Update main.py

--- main.py
+++ main.py
@@ -566,7 +566,7 @@ async def main():
     runner = web.AppRunner(app)
     await runner.setup()
 
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT"))
     site = web.TCPSite(runner, "0.0.0.0", port)
     await site.start()
 
